740d85397ec877ef20c046eb6a80b48efcf4f8f8
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-02-26 14:52:32 +0100

    hamburger click function

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const navBtn = document.querySelector('.nav');
+const navBtn = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.burger');
+
 
 navBtn.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 })
+
+$(document).ready(function(){
+    $(".hamburger").click(function(){
+      $(this).toggleClass("is-active");
+    });
+});

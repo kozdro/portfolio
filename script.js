46239ed05d8a3bd2f3669be10bc4bd3187b07355
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-03-04 14:26:15 +0100

    update scrolltrigger

--- script.js
+++ script.js
@@ -78,9 +78,15 @@ document.body.addEventListener('click', () => {
 
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.fromTo(".intro-img", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 })
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro-slider", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("header", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo("header nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".intro-title", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".intro-subtitle", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".intro-img", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".intro + section", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 
 gsap.registerPlugin(ScrollTrigger);
 
